7ce41f25f58031a7db8eae0a34544ca3161cf12e
Will Marshall <willmarshall2424@gmail.com>
2025-01-22 10:36:50 -0500

    init

---
+++ script.js
@@ -0,0 +1,64 @@
+
+
+
+
+const App = {
+
+    $: {
+        radBtn1: document.getElementById("radio1"),
+        radBtn2: document.getElementById("radio2"),
+        radBtn3: document.getElementById("radio3"),
+        radBtn4: document.getElementById("radio4"),
+
+        time: 0
+
+    },
+    
+    
+    
+    init(){
+        this.timer()
+        this.applyListeners()
+    },
+    
+    timer(){
+        App.$.time = setInterval(this.slidechange, 8000);
+    },
+    
+    resetTimer(){
+        clearInterval(this.$.time)
+        App.$.time = setInterval(this.slidechange, 8000);
+    },
+    
+    applyListeners(){
+        const btns = [App.$.radBtn1, App.$.radBtn2, App.$.radBtn3, App.$.radBtn4]
+       
+        btns.forEach((btn) => 
+            btn.addEventListener("click", (e) => {
+                App.resetTimer();
+            })
+        )
+    },
+    
+    slidechange() {
+  
+    if(App.$.radBtn1.checked === true){
+        App.$.radBtn2.checked = true;
+    }
+    else if(App.$.radBtn2.checked === true){
+        App.$.radBtn3.checked = true;
+    }
+    else if(App.$.radBtn3.checked === true){
+        App.$.radBtn4.checked = true;
+    }
+    else {
+        App.$.radBtn1.checked = true;
+    }
+    
+    }
+
+}
+
+
+
+App.init()
